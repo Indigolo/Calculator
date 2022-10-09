d8a9a1e0819bd33374ce40e7963a3991e58b562c
indigolo <jijavadzeachiko@gmail.com>
2022-10-09 02:14:33 +0200

    Create basic priority, can't do bracket in bracket

--- script.js
+++ script.js
@@ -1,9 +1,107 @@
 let answer = '';
 
-function action(num1, num2){
+function calculation(arithmetic){
+    for(let i = 0; i < arithmetic.length; i++){
+        switch(arithmetic[i]){
+            case '%':
+                answer = arithmetic[i - 1] % arithmetic[i + 1];
+                arithmetic.splice(i - 1, 3, answer);
+                i = 0;
+                break;
 
+            case 'x':
+                answer = arithmetic[i - 1] * arithmetic[i + 1];
+                arithmetic.splice(i - 1, 3, answer);
+                i = 0;
+                break;
+
+            case '/':
+                answer = arithmetic[i - 1] / arithmetic[i + 1];
+                arithmetic.splice(i - 1, 3, answer);
+                i = 0;
+                break;
+        }
+    }
+
+    for(let i = 0; i < arithmetic.length; i++){
+        switch(arithmetic[i]){
+            case '+':
+                answer = arithmetic[i - 1] + arithmetic[i + 1];
+                arithmetic.splice(i - 1, 3, answer);
+                i = 0;
+                break;
+
+            case '-':
+                answer = arithmetic[i - 1] - arithmetic[i + 1];
+                arithmetic.splice(i - 1, 3, answer);
+                i = 0;
+                break;
+        }
+    }
 }
 
+
+function calculation2(arithmetic){
+    let tempAnswer = 0;
+
+    let tempArithmetic = arithmetic;
+
+    for(let i = 0; i < tempArithmetic.length; i++){
+        switch(tempArithmetic[i]){
+            case '%':
+                tempAnswer = tempArithmetic[i - 1] % tempArithmetic[i + 1];
+                tempArithmetic.splice(i - 1, 3, tempAnswer);
+                i = 0;
+                break;
+
+            case 'x':
+                tempAnswer = tempArithmetic[i - 1] * tempArithmetic[i + 1];
+                tempArithmetic.splice(i - 1, 3, tempAnswer);
+                i = 0;
+                break;
+
+            case '/':
+                tempAnswer = tempArithmetic[i - 1] / tempArithmetic[i + 1];
+                tempArithmetic.splice(i - 1, 3, tempAnswer);
+                i = 0;
+                break;
+        }
+    }
+
+    for(let i = 0; i < tempArithmetic.length; i++){
+        switch(tempArithmetic[i]){
+            case '+':
+                tempAnswer = tempArithmetic[i - 1] + tempArithmetic[i + 1];
+                tempArithmetic.splice(i - 1, 3, tempAnswer);
+                i = 0;
+                break;
+
+            case '-':
+                tempAnswer = tempArithmetic[i - 1] - tempArithmetic[i + 1];
+                tempArithmetic.splice(i - 1, 3, tempAnswer);
+                i = 0;
+                break;
+        }
+    }
+
+    return tempAnswer;
+}
+
+let leftIndex = [];
+let rightIndex = [];
+
+function countPriority(arithmetic){
+    leftIndex = [];
+    rightIndex = [];
+
+    for(let i = 0; i < arithmetic.length; i++){
+        if(arithmetic[i] == '(')
+        leftIndex.push(i);
+
+        else if(arithmetic[i] == ')')
+        rightIndex.push(i);
+    }
+}
 
 const result = document.querySelector('#result');
 const buttons = document.querySelectorAll('button');
@@ -29,50 +127,20 @@ enter.addEventListener('click', () => {
     let arithmetic = result.textContent.split(' ');
     arithmetic = arithmetic.filter((char) => {return (char !== '')});
 
-    let leftIndex = [];
-    let rightIndex = [];
+    countPriority(arithmetic);
 
-    for(let i = 0; i < arithmetic.length; i++){
-        if(arithmetic[i] == '('){
-            leftIndex.push(i);
-        }
+    if(leftIndex.length > 0 && leftIndex.length == rightIndex.length){
 
-        else if(arithmetic[i] == ')'){
-            rightIndex.push(i);
-        }
-    }
+        while(rightIndex.length > 0){
+            let priority = arithmetic.slice(leftIndex[0] + 1, rightIndex[0]);
 
-    if(leftIndex && rightIndex){
-        let priorityAnswer = [];
+            console.log(arithmetic)
+            console.log(priority)
 
-        for(let i = 0; i < rightIndex.length; i++){
-            let priority = arithmetic.slice(leftIndex[i] + 1 - 4 * i, rightIndex[i] - 4 * i);
+            arithmetic.splice(leftIndex[0], rightIndex[0] - leftIndex[0] + 1, calculation2(priority));
 
-            switch(priority[1]){
-                case '+':
-                    priorityAnswer[i] = priority[0] + priority[2];
-                    break;
-                
-                case '-':
-                    priorityAnswer[i] = priority[0] - priority[2];
-                    break;
-                
-                case 'x':
-                    priorityAnswer[i] = priority[0] * priority[2];
-                    break;
-                
-                case '/':
-                    priorityAnswer[i] = priority[0] / priority[2];
-                    break;
-                
-                case '%':
-                    priorityAnswer[i] = priority[0] % priority[2];
-                    break;
-            }
-
-            arithmetic.splice(leftIndex[i] - 4 * i, rightIndex[i] - leftIndex[i] + 1, priorityAnswer[i] - 4 * i);
-
-        }
+            countPriority(arithmetic);
+        }     
     }
 
     if(arithmetic.length == 1){
@@ -80,29 +148,7 @@ enter.addEventListener('click', () => {
     }
 
     else{
-        for(let i = 0; i < arithmetic.length; i++){
-            switch(arithmetic[i]){
-                case '+':
-                    answer = arithmetic[i - 1] + arithmetic[i + 1];
-                    break;
-    
-                case '-':
-                    answer = arithmetic[i - 1] - arithmetic[i + 1];
-                    break;
-    
-                case 'x':
-                    answer = arithmetic[i - 1] * arithmetic[i + 1];
-                    break;
-    
-                case '/':
-                    answer = arithmetic[i - 1] / arithmetic[i + 1];
-                    break;
-    
-                case '%':
-                    answer = arithmetic[i - 1] % arithmetic[i + 1];
-                    break;
-            }
-        }
+        answer = calculation2(arithmetic);
     }
 
     result.textContent = Number(answer);
